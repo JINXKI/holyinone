ccd1c8fae40969d4880a4976b79cb70508952bd5
JINXKI <jins99seo@gmail.com>
2023-08-31 02:08:14 +0900

    Welcome Page Connected

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Welcome from './screens/Welcome'; 
+// import Navigation from './navigation/index';
+
+import Welcome from './screens/Welcome';
 
 export default function App() {
-
-  var cnt = 0;
 
   return (
     <View style={styles.container}>
--- navigation/index.js
+++ navigation/index.js
@@ -3,28 +3,28 @@ import { Image } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import Welcome from '../screens/Welcome';
-import Signup from '../screens/Signup';
-import Login from '../screens/Login';
-import Forgot from '../screens/Forgot';
-import Bible from '../screens/Bible';
-import Hymn from '../screens/Hymn';
-import Note from '../screens/Note';
-import Settings from '../screens/Settings';
-import EditProfile from '../screens/EditProfile';
+// import Signup from '../screens/Signup';
+// import Login from '../screens/Login';
+// import Forgot from '../screens/Forgot';
+// import Bible from '../screens/Bible';
+// import Hymn from '../screens/Hymn';
+// import Note from '../screens/Note';
+// import Settings from '../screens/Settings';
+// import EditProfile from '../screens/EditProfile';
 
 import { theme } from '../constants';
 
 const screens = createStackNavigator(
     {
         Welcome,
-        Signup,
-        Login,
-        Forgot,
-        Bible,
-        Hymn,
-        Note,
-        Settings,
-        EditProfile,
+        // Signup,
+        // Login,
+        // Forgot,
+        // Bible,
+        // Hymn,
+        // Note,
+        // Settings,
+        // EditProfile,
     },
     {
         defaultNavigationOptions: {
